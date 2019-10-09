08fa4ac81bd74a1f57ab774272dcb45c41121bca
Jonathan Norman <jnorman2123@gmail.com>
2019-10-09 01:33:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,14 @@ def reverse_each_word(sentence)
   sentence = reversed_array.join(" ")
   p sentence
 end
+
+def reverse_each_word(sentence)
+  array = []
+  reversed_array = []
+  array = sentence.split
+  array.each do |word|
+     reversed_array << word.reverse
+  end
+  sentence = reversed_array.join(" ")
+  p sentence
+end
